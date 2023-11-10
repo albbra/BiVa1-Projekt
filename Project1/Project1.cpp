b69a02b3598ebfcbd7577374211e4a3dc3004754
albbra <Albbrandt1@gmail.com>
2023-11-10 22:47:18 +0100

    init

---
+++ Project1/Project1.cpp
@@ -0,0 +1,12 @@
+﻿// Project1.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Project1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
